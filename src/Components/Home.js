7d342f16fe0c45fec716a8186c878a6286985cb9
Asick Ahamed <ashiq.it@gmail.com>
2019-12-27 16:06:38 +0100

    commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,73 +2,118 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import ReadMoreReact from 'read-more-react';
 import Rating from '@material-ui/lab/Rating';
-import StarRatingComponent from 'react-star-rating-component';
-
-
-
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import StarRatingComponent from 'react-star-rating-component'
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl'
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/';
 
 export default class Home extends Component {
-    // constructor(props){
-    //     super(props)
-    //     this.state = {
-    //       movielists: []
-    //     }
-    //   }
+    constructor(props){
+        super(props)
+        this.state = {
+          movies: [],
+          editMovieData: {
+            id: "",
+            rating: ""
+          },
+          filter:""
+        }
+        
+      }
+ 
 
-    state = {
-        movies: [],
-        newMovieData: {
-          title: "",
-          rating: ""
-        },
-        editMovieData: {
-          id: "",
-          rating: ""
-        },
-        newMovieModal: false,
-        editEditModal: false
-      };
-
-      componentDidMount(){
+       componentWillMount(){
          
          this._refreshMovies();
       }
 
       _refreshMovies() {
-        axios.get("https://localhost:3000/results").then(response => {
+        axios.get("http://localhost:3000/results").then(response => {
           this.setState({
             movies: response.data
           });
         });
       }
 
+      filter(e){
+        
+        this.setState({filter: e.target.value})
+      }
+    
+      filterChamp(e){
+         
+        this.setState({filter: e.target.value})
+      }
+
+     
+
+      updateMovie() {
+        let {  rating } = this.state.editMovieData;
+        
+        axios
+          .put("http://localhost:3000/results/" + this.state.editMovieData.id, {
+            rating
+          })
+          .then(response => {
+            this._refreshBooks();
+            this.setState({
+              editBookData: {
+                id: "",
+                rating: ""
+              }
+            });
+             
+          });
+      }
+
 
       onStarClick(nextValue, prevValue, name) {
+        
         const movie_id = name.split("-").pop() // remove string
-         
-        //this.setState({id:movie_id,rating: nextValue});
+        console.log(this.state)
         this.setState({
             editMovieData: { id:movie_id,rating: nextValue }
           });
 
-        console.log(this.state)
       }
       
 
     render() {
-        console.log(this.state.movies)
+  
         var  getData =  this.state.movies;
+        if(this.state.filter){
+            
+            if(this.state.filter == 'desc'){
+            getData.sort((a, b) => b.rating - a.rating);
+            } 
+            else if(this.state.filter == 'asc'){
+            getData.sort((a, b) => a.rating - b.rating);    
+            } else if(this.state.filter == '0'){
+                getData.sort((a, b) => a.id - b.id);    
+            }
+        }  
         const minimumLength = 50
         const idealLength = 100
         const maxLength = 200
-
+         
+         
+         
         return (
             <div className="container movielist">
             <h3>Movie List</h3>
+        <div className="row">
+        <div className="col s3">
+        <label>Sort By</label>
+        <select onChange={this.filterChamp.bind(this)} value={this.state.filterChamp} className="browser-default">
+          <option value="0">Select</option>
+          <option value="desc">Rating Descending</option>
+          <option value="asc">Rating Ascending</option>
+        </select>
+         </div>
+        </div>
+
              <div className="row" >
             {getData.map((gh, i)=>(
             
@@ -87,7 +132,7 @@ export default class Home extends Component {
                 readMoreText="Read more..."/>
             <div className="card-action">
                  
-            {gh.rating}
+            
          <StarRatingComponent 
           name= { "movie-id-" + gh.id }  
           starCount={5}
