9a31d06c52fa162503e9b7cd739f8d15dcedd75b
Asick Ahamed <ashiq.it@gmail.com>
2019-12-30 16:08:32 +0100

    Update movies_ratings.sql

--- sql/movies_ratings.sql
+++ sql/movies_ratings.sql
@@ -1,7 +1,7 @@
 CREATE TABLE movies_ratings (
-	id SERIAL primary key,
+  id SERIAL primary key,
   title VARCHAR(255),
   poster_path VARCHAR(255),
-	rating INTEGER default 1,
-	overview VARCHAR(348500)
+  rating INTEGER default 1,
+  overview VARCHAR(348500)
 );
